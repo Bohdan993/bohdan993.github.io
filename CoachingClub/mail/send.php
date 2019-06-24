429028a7b434b4a150e00114f11365dc90c3e9e6
Bohdan993 <46867179+Bohdan993@users.noreply.github.com>
2019-06-24 14:10:34 +0300

    Add files via upload
    Coaching Club landing

---
+++ CoachingClub/mail/send.php
@@ -0,0 +1,39 @@
+<?php
+if ( !empty($_POST['phone']) && !empty($_POST['subject']) ) {
+  $return_message = "";
+
+  $name           = trim(htmlspecialchars($_POST['name']));
+  $phone          = trim(htmlspecialchars($_POST['phone']));
+  $subject        = trim(htmlspecialchars($_POST['subject']));
+  $subject_letter = 'Заявка с сайта Коучинг';
+
+  $to = "leila.emi7@gmail.com";
+
+  $message = "
+  <html> 
+      <head> 
+          <title>$subject_letter</title> 
+      </head> 
+      <body>
+        <table>
+          <tr><td><b>Тема:</b></td><td>$subject</td></tr>
+          <tr><td><b>Имя:</b></td><td>$name</td></tr>
+          <tr><td><b>Телефон:</b></td><td>$phone</td></tr>
+        </table>
+      </body> 
+  </html>"; 
+
+  $headers  = "From: noreply@kouching.com.ua\r\n";
+  $headers .= "Content-type: text/html; charset=utf-8 \r\n";
+
+  if (mail($to, $subject_letter, $message, $headers)) {
+    $return_message = "send_success";
+  }
+  else {
+    $return_message = "send_error";
+  }
+  echo $return_message;
+  exit();
+}
+
+?>
